e298e7b24a892db00b3db86ab307f1a94baae14b
Casey <leptone55@gmail.com>
2023-06-27 22:17:41 -0700

    use api

--- src/utils/getBears.ts
+++ src/utils/getBears.ts
@@ -6,10 +6,10 @@ type Response = {
 
 export default async function getBears(offset: number = 0) {
   const res = await fetch(
-    `http://localhost:3000/bear?offset=${offset}&limit=20`
+    // `http://localhost:3000/bear?offset=${offset}&limit=20`
     // `http://localhost:3000/bear?offset=${offset}&limit=20`
     // 'https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=oka%20y_bears'
-    // `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}&nextCursor=zmgsJaQ2HYMtaoS7GFdk3a1rUwekHkfVpmfrHZo7r93`
+    `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}&nextCursor=zmgsJaQ2HYMtaoS7GFdk3a1rUwekHkfVpmfrHZo7r93`
   )
 
   if (!res.ok) {
